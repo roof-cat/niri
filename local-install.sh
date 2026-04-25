4883ae5ea732aafddb5aaa6f81a29fea19d1845e
roof-cat <96749530+roof-cat@users.noreply.github.com>
2026-04-25 21:05:17 +0300

    refactor: consolidate installation scripts and update binary install paths

--- local-install.sh
+++ local-install.sh
@@ -1,10 +1,15 @@
+#!/bin/sh
 #sudo pacman -S clang
+
+set -e
 
 cargo build --release
 
-sudo cp -f target/release/niri /usr/local/bin/niri
+sudo cp -f target/release/niri /usr/bin/niri
 sudo cp resources/niri-session /usr/local/bin/niri-session
-sudo cp resources/niri.desktop /usr/share/wayland-sessions
-sudo cp resources/niri.service /lib/systemd/user/
+sudo cp resources/niri.desktop /usr/share/wayland-sessions/
+sudo cp resources/niri.service /usr/lib/systemd/user/
 
-sudo chmod +x /usr/local/bin/niri /usr/local/bin/niri-session
+sudo chmod +x /usr/bin/niri /usr/local/bin/niri-session
+
+echo "Niri installed, restart Niri"
--- local-update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cargo build --release
-
-sudo cp -f target/release/niri /usr/local/bin/niri
-sudo chmod +x /usr/local/bin/niri
-
-echo "Niri has been updated, restart Niri"
